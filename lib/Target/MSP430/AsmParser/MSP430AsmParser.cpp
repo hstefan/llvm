2ff6e3dc492e4d3beef845326be5d9f6eca70f6e
Hugo Puhlmann <hugopuhlmann@gmail.com>
2015-02-21 01:55:55 -0200

    [MSP430] Adding stubs for asm parser implementation.

---
+++ lib/Target/MSP430/AsmParser/MSP430AsmParser.cpp
@@ -0,0 +1,12 @@
+//===-- MSP430AsmParser.cpp - Parse Sparc assembly to MCInst instructions --===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#define GET_ASSEMBLER_HEADER
+
+#include "MSP430GenAsmMatcher.inc"
